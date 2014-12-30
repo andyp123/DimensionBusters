ae7e84ca1caa51b5ff56f216471e8d273d8c1ecd
Andrew Palmer <andyp.123@gmail.com>
2014-12-31 01:42:09 +0900

    Real initial commit
    uploading bare bones project with simple level and working networking.

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour
+{
+	public GameObject LevelCamera = null;
+	SpawnPoint[] spawnPoints = null;
+
+	void Start()
+	{
+		spawnPoints = GameObject.FindObjectsOfType<SpawnPoint>();
+		Connect();
+	}
+
+	void Connect()
+	{
+		PhotonNetwork.ConnectUsingSettings("DimensionBusters 0.0.1");
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+
+	void OnJoinedLobby()
+	{
+		Debug.Log("Joined Lobby");
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	void OnPhotonRandomJoinFailed()
+	{
+		Debug.Log("Creating Room");
+		PhotonNetwork.CreateRoom(null);
+	}
+
+	void OnJoinedRoom()
+	{
+		SpawnMyPlayer();
+	}
+
+	void SpawnMyPlayer()
+	{
+		// find spawn point
+		if(spawnPoints == null)
+		{
+			Debug.LogError("No spawnpoints in level");
+		}
+		SpawnPoint spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+		// instantiate local player and enable relevant components
+		GameObject player = (GameObject)PhotonNetwork.Instantiate("PlayerController", spawnPoint.transform.position, spawnPoint.transform.rotation, 0);
+		((MonoBehaviour)player.GetComponent("CharacterMotor")).enabled = true;
+		((MonoBehaviour)player.GetComponent("FPSInputController")).enabled = true;
+		((MonoBehaviour)player.GetComponent("MouseLook")).enabled = true;
+		player.transform.FindChild("Camera").gameObject.SetActive(true);
+
+		//disable level camera
+		LevelCamera.SetActive(false);
+	}
+}
---
+++ Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkPlayer : Photon.MonoBehaviour
+{
+	Vector3 netPosition;
+	Quaternion netRotation;
+
+	void Update()
+	{
+		if(!photonView.isMine)
+		{
+			// this is bad network code, since it's adding lag, instead of predicting a position
+			// based on velocity and reducing the effect of lag.
+			transform.position = Vector3.Lerp(transform.position, netPosition, 0.1f);
+			transform.rotation = Quaternion.Lerp(transform.rotation, netRotation, 0.1f);
+		}
+	}
+
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if(stream.isWriting) // local player : send data
+		{
+			stream.SendNext(transform.position);
+			stream.SendNext(transform.rotation);
+		}
+		else // remote player : receive data
+		{
+			netPosition = (Vector3)stream.ReceiveNext();
+			netRotation = (Quaternion)stream.ReceiveNext();
+		}
+	}
+}
